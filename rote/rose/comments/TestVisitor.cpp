565eefedbcf3e1a293bf94c72d6408cb34aae8c3
Geoff Hulette <ghulette@gmail.com>
2011-04-06 16:14:46 -0700

    Rose example to print comments

---
+++ rote/rose/comments/TestVisitor.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "rose.h"
+#include "TestVisitor.h"
+
+#define CPP_COMMENT (1)
+#define C_COMMENT   (2)
+
+using namespace std;
+
+void TestVisitor::visit(SgNode *node) {
+  SgLocatedNode *locatedNode = isSgLocatedNode(node);
+  if(locatedNode != NULL) {
+    AttachedPreprocessingInfoType *preprocInfo;
+    preprocInfo = locatedNode->getAttachedPreprocessingInfo();
+    if(preprocInfo != NULL) {
+      AttachedPreprocessingInfoType::iterator i;
+      for(i=preprocInfo->begin();i != preprocInfo->end(); i++) {
+        switch ((*i)->getTypeOfDirective()) {
+          case C_COMMENT:
+          case CPP_COMMENT:
+            cout << node->class_name() << endl;
+            cout << (*i)->getString() << endl;
+            break;
+        }
+      }
+    }
+  }
+}
