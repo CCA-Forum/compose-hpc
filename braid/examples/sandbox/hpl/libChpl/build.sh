ceb97bd4df1847d794058a9c85b0970b25ee41ef
Shams Imam <imam1@llnl.gov>
2011-07-06 15:54:51 -0700

    -- added gasnet build commands  - gasnet mode doesn't work yet, its a wip.

--- braid/examples/sandbox/hpl/libChpl/build.sh
+++ braid/examples/sandbox/hpl/libChpl/build.sh
@@ -12,6 +12,9 @@ CHAPEL_HOME="${CHPL_HOME}"
 CHAPEL_COMM="${CHPL_COMM}"
 CHAPEL_HOST_PLATFORM="${CHPL_HOST_PLATFORM}"
 CHAPEL_SUBSTRATE_DIR="${CHPL_HOME}/lib/${CHPL_HOST_PLATFORM}/gnu/comm-none/substrate-none"
+if [ "$CHPL_COMM" = "gasnet" ] ; then
+  CHAPEL_SUBSTRATE_DIR="${CHAPEL_HOME}/lib/${CHAPEL_HOST_PLATFORM}/gnu/comm-gasnet-nodbg/substrate-udp"
+fi
 CHAPEL_RUNTIME_INC_DIR="${CHPL_HOME}/runtime/include"
 
 SIDL_RUNTIME="/Users/imam1/softwares/include"
@@ -19,9 +22,23 @@ INCLUDES="`babel-config --includes` -I. -I${CHAPEL_RUNTIME_INC_DIR} -I${SIDL_RUN
 CFLAGS="`babel-config --flags-c` -std=c99"
 LIBS="`babel-config --libs-c-client`"
 
-CHPL_FLAGS="-D_POSIX_C_SOURCE  -DCHPL_TASKS_H=\"tasks-fifo.h\"  -DCHPL_THREADS_H=\"threads-pthreads.h\" -I${CHAPEL_RUNTIME_INC_DIR}/tasks/fifo  -I${CHAPEL_RUNTIME_INC_DIR}/threads/pthreads -I${CHAPEL_RUNTIME_INC_DIR}/comm/none -I${CHAPEL_RUNTIME_INC_DIR}/comp-gnu -I${CHAPEL_RUNTIME_INC_DIR}/${CHAPEL_HOST_PLATFORM} -I${CHAPEL_RUNTIME_INC_DIR} -I. -Wno-all"
+COMM_INCLUDE="${CHAPEL_RUNTIME_INC_DIR}/comm/none"
+if [ "$CHPL_COMM" = "gasnet" ] ; then
+  COMM_INCLUDE="${CHAPEL_RUNTIME_INC_DIR}/comm/gasnet"
+fi
+GASNET_FLAGS=""
+if [ "$CHPL_COMM" = "gasnet" ] ; then
+  GASNET_FLAGS="-no-cpp-precomp   -DGASNET_PAR -D_REENTRANT   -I${CHAPEL_HOME}/third-party/gasnet/install/${CHAPEL_HOST_PLATFORM}-gnu/seg-everything/nodbg/include  -I${CHAPEL_HOME}/third-party/gasnet/install/${CHAPEL_HOST_PLATFORM}-gnu/seg-everything/nodbg/include/udp-conduit"
+fi
+
+CHPL_FLAGS="-D_POSIX_C_SOURCE  -DCHPL_TASKS_H=\"tasks-fifo.h\"  -DCHPL_THREADS_H=\"threads-pthreads.h\" -I${CHAPEL_RUNTIME_INC_DIR}/tasks/fifo  -I${CHAPEL_RUNTIME_INC_DIR}/threads/pthreads -I${COMM_INCLUDE} -I${CHAPEL_RUNTIME_INC_DIR}/comp-gnu -I${CHAPEL_RUNTIME_INC_DIR}/${CHAPEL_HOST_PLATFORM} -I${CHAPEL_RUNTIME_INC_DIR} -I. -Wno-all"
+
 CHPL_LDFLAGS="-L${CHAPEL_SUBSTRATE_DIR}/tasks-fifo/threads-pthreads ${CHAPEL_SUBSTRATE_DIR}/tasks-fifo/threads-pthreads/main.o -lchpl  -lm  -lpthread"
 GASNET_LDFLAGS=""
+if [ "$CHPL_COMM" = "gasnet" ] ; then
+  GASNET_LDFLAGS="-L${CHAPEL_HOME}/third-party/gasnet/install/${CHAPEL_HOST_PLATFORM}-gnu/seg-everything/nodbg/lib  -lgasnet-udp-par -lamudp -lgcc "
+fi
+
 EXTRA_LDFLAGS=""
 
 # extra include/compile flags
@@ -33,7 +50,7 @@ PREFIX=/usr/local
 # the default installation installs the .la and .scl (if any) into the LIBDIR
 LIBDIR=${PREFIX}/lib
 
-BABEL_LIBTOOL_COMMAND="babel-libtool --mode=compile --tag=CC ${CC} -I./gen ${INCLUDES} ${CFLAGS} ${EXTRAFLAGS} ${CHPL_FLAGS}"
+BABEL_LIBTOOL_COMMAND="babel-libtool --mode=compile --tag=CC ${CC} -I./gen ${INCLUDES} ${CFLAGS} ${EXTRAFLAGS} ${GASNET_FLAGS} ${CHPL_FLAGS}"
 
 # Generate the C source files for the chapel program
 # chpl --savec ./gen *.chpl 
@@ -55,8 +72,17 @@ ${BABEL_LIBTOOL_COMMAND} -c  -o ./gen/hplsupport_SimpleArray1dInt_cImpl.c.o   hp
 ${BABEL_LIBTOOL_COMMAND} -c  -o ./gen/hplsupport_HPL_cClient.c.o  hplsupport_HPL_cClient.c
 
 # Compile the chapel generated C source files and generate the object files
-# ${CC_COMMAND}  ${CHPL_FLAGS}  -c  -o ./gen/a.out.tmp.o   ./gen/_main.c 
-${BABEL_LIBTOOL_COMMAND} -c  -o ./gen/a.out.tmp.o   ./gen/_main.c
+if [ "$CHPL_COMM" = "gasnet" ] ; then
+
+    ${BABEL_LIBTOOL_COMMAND}  -c  -o ./gen/a.out.tmp.o   ./gen/_main.c 
+
+else
+
+  echo "CHPL_COMM is NOT gasnet"
+  ${BABEL_LIBTOOL_COMMAND} -c  -o ./gen/a.out.tmp.o   ./gen/_main.c
+
+fi
+
  
 MODFLAG=""
 # MODFLAG="module"            
@@ -81,5 +107,29 @@ babel-libtool --mode=link ${CXX} -static \
   ./gen/hplsupport_HPL_cClient.c.o \
   ./gen/a.out.tmp.o 
     
-cp ./gen/a.out.tmp a.out
-rm ./gen/a.out.tmp
+    
+if [ "$CHPL_COMM" = "gasnet" ] ; then
+
+  cp ./gen/a.out.tmp a.out_real
+  rm ./gen/a.out.tmp
+  echo "#include \"chplcgfns.h\"" > ./gen/config.c
+  echo "#include \"config.h\"" >> ./gen/config.c
+  echo "#include \"_config.c\"" >> ./gen/config.c
+
+  ${CC} -std=c99 -D_POSIX_C_SOURCE   -c -o ./gen/a.out.tmp_launcher.o -I${CHAPEL_RUNTIME_INC_DIR}/${CHAPEL_HOST_PLATFORM} -I${CHAPEL_RUNTIME_INC_DIR} -I. ./gen/config.c
+  ${CC}   -o ./gen/a.out.tmp_launcher -L${CHAPEL_SUBSTRATE_DIR}/launch-amudprun ./gen/a.out.tmp_launcher.o ${CHAPEL_SUBSTRATE_DIR}/launch-amudprun/main_launcher.o -lchpllaunch -lm 
+
+  cp ./gen/a.out.tmp_launcher ./gen/a.out.tmp
+  cp ./gen/a.out.tmp a.out
+  rm ./gen/a.out.tmp
+
+
+else
+
+  cp ./gen/a.out.tmp a.out
+  rm ./gen/a.out.tmp
+
+fi
+
+
+
