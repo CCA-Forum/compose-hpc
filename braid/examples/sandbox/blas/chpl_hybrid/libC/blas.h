f6dcbed9160e8a0a98ec932ad7c7691a623bc8c7
Adrian V. Prantl <prantl1@zeus287.llnl.gov>
2011-07-19 16:52:39 -0700

    -- commit for working version of blas example on zeus

--- braid/examples/sandbox/blas/chpl_hybrid/libC/blas.h
+++ braid/examples/sandbox/blas/chpl_hybrid/libC/blas.h
@@ -2,7 +2,7 @@
  * File:          blas.h
  * Symbol:        blas-v0.1
  * Symbol Type:   package
- * Babel Version: 2.0.0 (Revision: 0  )
+ * Babel Version: 2.0.0 (Revision: 7138  trunk)
  * Description:   Client-side glue code for blas
  * 
  * WARNING: Automatically generated; changes will be lost
