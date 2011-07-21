2f0b55d4c46cb989474b559849323b0531800380
Adrian V. Prantl <prantl1@ansel0.llnl.gov>
2011-07-21 13:19:57 -0700

    -- script generators

--- braid/examples/sandbox/blas/moab_script_gen.py
+++ braid/examples/sandbox/blas/moab_script_gen.py
@@ -1,50 +1,48 @@
 #!/usr/bin/python
 
-f = 1
-blockSize = f * 100
+p = "pbatch"
+
+blockSize = 100
 numElems = 16 * blockSize
 maxNodes = 16
-numIter = 4
+numRuns = 3
 
-writeln("# echo Num Elements = ", numElems);
-writeln("# echo Block Size = ", blockSize);
-
-for t in range(1,5):
+for t in range(1,100,50):
   n = t * numElems
   b = t * blockSize
 
-  for r in range(1,numIter+1):
+  for r in range(1,numRuns+1):
     for i in range(2, maxNodes, 2):
         fh = open('data_hybrid-{i}-{n}-{b}-{r}.moab'.format(i=i,n=n,b=b,r=r), 'w')
         fh.write('''
 #MSUB -A comp
-#MSUB -l nodes=4
+#MSUB -l nodes={i}
 #MSUB -l partition=ansel
-#MSUB -l walltime=0:05:00
-#MSUB -q pdebug
-#MSUB -m be
-#MSUB -N ptrans
+#MSUB -l walltime=0:30:00
+#MSUB -q {p}
+#MSUB -m a
+#MSUB -N hy_{i}-{n}-{b}-{r}
 #MSUB -V
 #MSUB -o /p/lscratchb/prantl1/data_hybrid-{i}-{n}-{b}-{r}.txt
 
-export MPIRUN_CMD="srun -N %%N %%P %%A"
-chpl_hybrid/runChapel/runChapel2C -nl {i} -v --rowSize={n} --blkSize={b}
-'''.format(i=i,n=n,b=b,r=r))
+export MPIRUN_CMD="srun -N %N %P %A"
+chpl_hybrid/runChapel/runChapel2C -nl {i} -v --rowSize=32 --colSize={n} --blkSize={b} --maxThreadsPerLocale=12 --dataParTasksPerLocale=12
+'''.format(i=i,n=n,b=b,r=r,p=p))
         fh.close()
 
         fh = open('data_pure-{i}-{n}-{b}-{r}.moab'.format(i=i,n=n,b=b,r=r), 'w')
         fh.write('''
 #MSUB -A comp
-#MSUB -l nodes=4
+#MSUB -l nodes={i}
 #MSUB -l partition=ansel
-#MSUB -l walltime=0:05:00
-#MSUB -q pdebug
-#MSUB -m be
-#MSUB -N ptrans
+#MSUB -l walltime=0:30:00
+#MSUB -q {p}
+#MSUB -m a
+#MSUB -N pu_{i}-{n}-{b}-{r}        
 #MSUB -V
 #MSUB -o /p/lscratchb/prantl1/data_pure-{i}-{n}-{b}-{r}.txt
 
-export MPIRUN_CMD="srun -N %%N %%P %%A"
-chpl_pure/runMe -nl {i} -v --rowSize={n} --blkSize={b} --maxThreadsPerLocale=12 --dataParTasksPerLocale=12
-'''.format(i=i,n=n,b=b,r=r))
+export MPIRUN_CMD="srun -N %N %P %A"
+chpl_pure/runMe -nl {i} -v --rowSize=32 --colSize={n} --blkSize={b} --maxThreadsPerLocale=12 --dataParTasksPerLocale=12
+'''.format(i=i,n=n,b=b,r=r,p=p))
         fh.close()
--- braid/examples/sandbox/blas/script_gen.chpl
+++ braid/examples/sandbox/blas/script_gen.chpl
@@ -1,20 +1,31 @@
 
 
 config const f: int(32) = 1;
-config const b: int(64) = f * 10000;
-config const n: int(64) = 16 * b;
+config const blockSize: int(64) = f * 100;
+config const numElems: int(64) = 16 * blockSize;
+config const maxNodes = 16;
+config const numIter = 4;
 
 writeln("#!/bin/sh");
 
-writeln("echo Num Elements = ", n);
-writeln("echo Block Size = ", b);
+writeln("# echo Num Elements = ", numElems);
+writeln("# echo Block Size = ", blockSize);
 
-for i in [2 .. 12 by 2] do {
+for param t in 1 .. 5 do {
+  var n = t * numElems;
+  var b = t * blockSize;
 
-  writeln("chpl_hybrid/runChapel/runChapel2C -nl ", i, " -v --numElements=", n, " --blkSize=", b, " &> data_hybrid-", i, "-", n, "-", b, ".txt &" );
+  for r in [1 .. #numIter] do {
+    for i in [2 .. maxNodes by 2] do {
 
-  writeln("chpl_pure/runMe -nl ", i, " -v --numElements=", n, " --blkSize=", b, " --maxThreadsPerLocale=8 --dataParTasksPerLocale=8 &> data_pure-", i, "-", n, "-", b, ".txt &" );
+      writeln("chpl_hybrid/runChapel/runChapel2C -nl ", i, " -v --rowSize=", n, " --blkSize=", b, 
+        " &> data_hybrid", "-", i, "-", n, "-", b, "-", r, ".txt &" );
 
+      writeln("chpl_pure/runMe -nl ", i, " -v --rowSize=", n, " --blkSize=", b, 
+        " --maxThreadsPerLocale=8 --dataParTasksPerLocale=8 ",
+        "&> data_pure", "-", i, "-", n, "-", b, "-", r, ".txt &" );
+
+    }
+  }
 }
-
 
