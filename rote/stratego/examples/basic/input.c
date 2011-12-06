16c6845d6d9b4a28c6c2df00816346d0b7ad6c29
Geoff Hulette <ghulette@gmail.com>
2011-12-06 11:59:29 -0800

    Update

--- rote/stratego/examples/basic/input.c
+++ rote/stratego/examples/basic/input.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    int x,y,z; 
-    x = 5;
-    y = 3;
-    z = x + y;
+    int w,x,y,z; 
+    w = 1;
+    x = 3;
+    y = 5;
+    z = w * (x + y);
+    // rewrite to:
+    // z = w * x + w * y;
     printf("z=%d\n",z);
     return 0;
 }
--- rote/stratego/examples/basic/run.sh
+++ rote/stratego/examples/basic/run.sh
@@ -2,9 +2,7 @@
 
 c2term -i input.c > input.trm
 sed -i -e s/\'/\"/g input.trm
-pp-aterm -i input.trm -o input-pp.trm
 ./basic -i input.trm > output.trm
-pp-aterm -i output.trm -o output-pp.trm
 sed -i -e s/\"/\'/g output.trm
 term2c output.trm
 
