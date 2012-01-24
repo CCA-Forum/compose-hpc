951dcf0a6d938cd858abc3f48525fa97dc4fefed
Matt Sottile <matt@galois.com>
2012-01-23 23:26:22 -0800

    documentation stuff

--- paul/libpaul/src/PaulConfReader.h
+++ paul/libpaul/src/PaulConfReader.h
@@ -4,10 +4,39 @@
 #include <map>
 #include <string>
 
-using namespace std;
+/**
+ * The paul_tag_map type simply defines a mapping between tag names
+ * and the parser type.  In a PAUL structured comment, the format is:
+ *
+ *  %TAG [body]
+ *
+ * TAG can be any string that contains no whitespace.  All text following
+ * the TAG to the end of the comment is treated as text that can be
+ * consumed by the parser associated with comments of type TAG.
+ */
+typedef std::map<const std::string, std::string> paul_tag_map;
 
-typedef map<const string, string> paul_tag_map;
-
-paul_tag_map read_paul_conf(string fname);
+/**
+ * This function reads the specified PAUL configuration file and
+ * returns a paul_tag_map for use within the PaulDecorate code.
+ *
+ * PAUL config files are formatted as follows:  any line that begins with
+ * a semi-colon is treated as a comment and ignored.  Every other line is
+ * assumed to be formatted as:
+ *
+ *   TAG  parser-name
+ *
+ * Whitespace before the TAG is ignored.
+ *
+ * Valid parser-names include:
+ *
+ * - Plain
+ * - s-expression
+ * - key-value
+ *
+ * \param fname Filename of parameter file.
+ * \return The paul_tag_map structure.
+ */
+paul_tag_map read_paul_conf(std::string fname);
 
 #endif // __PAULCONFREADER_H__ 
--- paul/libpaul/src/PaulDecorate.h
+++ paul/libpaul/src/PaulDecorate.h
@@ -6,9 +6,19 @@
 
 using namespace std;
 
+/**
+ * This function is used to decorate a SgProject with parsed
+ * structured comments treated as AstAttributes.  The
+ * SgProject parameter must be initialized using the usual methods
+ * for creating an SgProject in ROSE.  The second parameter specifies
+ * the configuration file that contains the tag->parser mappings used
+ * by PAUL to determine which parser to invoke for each tag that it
+ * encounters.
+ *
+ * \param sageProject The SgProject to perform the decoration traversal on.
+ * \param conf_fname The filename of the PAUL configuration file.
+ */
 void paulDecorate (SgProject* sageProject, string conf_fname) ;
-
-extern string annotAttributeString;
 
 #endif
 
