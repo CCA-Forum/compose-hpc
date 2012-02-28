e99ec2bc62d11f0936e861385f0742135c8c6252
Adrian Prantl <adrian@llnl.gov>
2012-02-28 08:54:05 -0800

    make sure that there is a closing '.' at the end of the stratego output this fixes BUG 3493517

--- rote/stratego/examples/basic/run.sh
+++ rote/stratego/examples/basic/run.sh
@@ -4,5 +4,6 @@ src2term -i input.c > input.trm
 sed -i -e s/\'/\"/g input.trm
 ./basic -i input.trm > output.trm
 sed -i -e s/\"/\'/g output.trm
+echo '.' >>output.trm
 term2src output.trm
 
--- rote/stratego/examples/identity/run.sh
+++ rote/stratego/examples/identity/run.sh
@@ -6,5 +6,6 @@ pp-aterm -i input.trm -o input-pp.trm
 ./identity -i input.trm > output.trm
 pp-aterm -i output.trm -o output-pp.trm
 sed -i -e s/\"/\'/g output.trm
+echo '.' >>output.trm
 term2src output.trm
 
