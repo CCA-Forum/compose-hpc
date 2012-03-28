b4e782d9215da8ae463c88999668140e3d03da41
Ajay Panyala <ajay@csc.lsu.edu>
2012-03-28 16:00:43 -0700

    Changed the sed command in the run script for the  basic stratego transformation example.  - The sed command replaces , :: with , "::" - The extra space    was interfering with successful replacement.

--- rote/stratego/examples/basic/run.sh
+++ rote/stratego/examples/basic/run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 src2term -i input.c > input.trm
-sed -i -e s/\'/\"/g -e 's/, ::/, "::"/g' input.trm
+sed -i -e s/\'/\"/g -e 's/,::/,"::"/g' input.trm
 ./basic -i input.trm > output.trm
-sed -i -e s/\"/\'/g -e 's/, "::"/, ::/g' output.trm
+sed -i -e s/\"/\'/g -e 's/,"::"/,::/g' output.trm
 echo '.' >>output.trm
 term2src output.trm
 
