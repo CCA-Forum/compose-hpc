f88fd3c6a4658c1d385f003507d890b17d731386
Adrian Prantl <adrian@llnl.gov>
2012-04-05 13:55:37 -0700

    massaged the run.sh script to make stratego accept terms

--- rote/stratego/examples/fortran/run.sh
+++ rote/stratego/examples/fortran/run.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
-sed -i -e s/\'/\"/g input.trm
-./identity -i input.trm > output.trm
-sed -i -e s/\"/\'/g output.trm
-echo '.' >> output.trm
-term2src output.trm
+src2term -Iinput -i input/test.F -o input.term
+# WARNING
+# Please be aware that the replacement strings for ! and # may
+# theoretically conflict with a string that appears verbatim in the
+# sources.
+sed -i \
+    -e s/\"/\\\\\"/g \
+    -e s/\'/\"/g \
+    -e "s/,::/,\'::\'/g" \
+    -e 's/!/STRATEGO-BANG/g' \
+    -e 's/#/STRATEGO-OCTOTHORPE/g' \
+    input.term
+./identity -i input.term > output.term
+sed -i \
+    -e "s/\([^\\]\)\"/\\1\'/g" \
+    -e s/\\\\\"/\"/g \
+    -e 's/STRATEGO-BANG/!/g' \
+    -e 's/STRATEGO-OCTOTHORPE/#/g' \
+    output.term
+echo '.' >> output.term
+term2src output.term
 
