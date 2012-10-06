57bb349e94005b695155c55e7d952b2c5d1364c9
Matthew Sottile <mjsottile@me.com>
2012-10-06 01:23:38 -0700

    explanation of why file exists

--- rote/struct-diff/RuleGen/AtermUtilities.hs
+++ rote/struct-diff/RuleGen/AtermUtilities.hs
@@ -1,3 +1,19 @@
+{-
+
+Helper code to supplement the aterm package on hackage.
+All of our code is in terms of LabeledTrees, which are
+just what we call a forest of string trees using the
+tree and forest type from the standard Data.Tree package.
+The helpers here are useful to turn the aterms into
+these labeled trees.
+
+Note that this may not be optimal with respect to
+space for huge aterms.  That doesn't matter for the
+purposes of this prototype tool.
+
+- matt@galois.com
+
+-}
 module RuleGen.AtermUtilities (
 	readToTree
 ) where
