ce73e0b2942484bd44e4ebf4e74d8dcb738a3119
Matthew Sottile <mjsottile@me.com>
2012-11-01 17:45:16 -0700

    cleaning up to get rid of hardwired hostname and username.

--- rote/struct-diff/helpers/sourcify.sh
+++ rote/struct-diff/helpers/sourcify.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-echo "Fixing quotes."
-cat $1 | sed s/\"/\'/g > $1.tmp
+### set hostname (or username@hostname)
+: ${MINITERMITE_HOST:?"Need MINITERMITE_HOST set."}
 
-echo "Copying file to ftt."
-scp $1.tmp matt@ftt.dev.galois.com:sandbox/$1
-rm $1.tmp
+echo "Copying file."
+scp $1 ${MINITERMITE_HOST}:sandbox/$1
 
 echo "Running remote command."
-ssh matt@ftt.dev.galois.com "/home/matt/term2src.sh $1 $2" 
+ssh ${MINITERMITE_HOST} "/home/matt/term2src.sh $1 $2" 
 
 echo "Copying results back."
-scp matt@ftt.dev.galois.com:sandbox/$2 $2
+scp ${MINITERMITE_HOST}:sandbox/$2 $2
 
 echo "Cleaning up."
-ssh matt@ftt.dev.galois.com "rm /home/matt/sandbox/$2"
-ssh matt@ftt.dev.galois.com "rm /home/matt/sandbox/$1"
+ssh ${MINITERMITE_HOST} "rm /home/matt/sandbox/$2"
+ssh ${MINITERMITE_HOST} "rm /home/matt/sandbox/$1"
--- rote/struct-diff/helpers/termify.sh
+++ rote/struct-diff/helpers/termify.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-echo "Copying file to ftt."
-scp $1 matt@ftt.dev.galois.com:sandbox/$1
+### set hostname (or username@hostname)
+: ${MINITERMITE_HOST:?"Need MINITERMITE_HOST set."}
+
+echo "Copying file."
+scp $1 ${MINITERMITE_HOST}:sandbox/$1
 
 echo "Running remote command."
-ssh matt@ftt.dev.galois.com "/home/matt/src2term.sh $1" 
+ssh ${MINITERMITE_HOST} "/home/matt/src2term.sh $1" 
 
 echo "Copying results back."
-scp matt@ftt.dev.galois.com:sandbox/output.trm $2
+scp ${MINITERMITE_HOST}:sandbox/output.trm $2
 
-echo "Fixing quotes and trailing dot."
-cat $2 | sed s/\'/\"/g > $2.tmp
-mv $2.tmp $2
+echo "Fixing file to match aterm expectation."
 cat $2 | sed s/\.$//g > $2.tmp
 mv $2.tmp $2
 
-
 echo "Cleaning up."
-ssh matt@ftt.dev.galois.com "rm /home/matt/sandbox/output.trm"
-ssh matt@ftt.dev.galois.com "rm /home/matt/sandbox/$1"
+ssh ${MINITERMITE_HOST} "rm /home/matt/sandbox/output.trm"
+ssh ${MINITERMITE_HOST} "rm /home/matt/sandbox/$1"
