957f887a61dcb984fa7a834bc9d8336d860b57a8
Matthew Sottile <matt@galois.com>
2013-03-21 00:42:38 -0700

    documentation of config format

--- rote/struct-diff/RuleGen/Util/Configuration.hs
+++ rote/struct-diff/RuleGen/Util/Configuration.hs
@@ -1,3 +1,47 @@
+{-|
+  This module contains code for reading configuration files.  It uses the
+  format supported by Data.ConfigFile, which is fairly standard and similar to
+  that used by Windows .INI files.  The supported format is based on a config
+  file partitioned into a set of sections like this:
+
+  [section_name]
+  key=value
+  key=value
+  key=value
+
+  [other_section_name]
+  key=value
+  key=value
+
+  and so on...  Section names are arbitary and not interpreted - they simply
+  serve to name the blocks of key-value pairs that are meaningful.  The only
+  constraint is that section names must be unique.  For a given section name,
+  the first key that is interpreted is "phase".  This can take on values
+  "pre", "post", "generalize", or "context".  These correspond to the
+  different phases in the rule generator.  Pre-filters are substitution
+  filters applied before anything happens.  Post-filters are substitution
+  filters applied after everything is done and right before rule emission.
+  Generalize filters are used to indicate where and how generalization occurs.
+  Contextualize filters are used to indicate where holes (missing code on
+  one side or the other) are dealt with in the sense of rule generation.
+  The remaining key/value pairs in a section depend on the phase.
+
+  Phase: pre
+    match=val1, val2, ... , valN :: seek nodes with one of these labels
+    substitute=val               :: replace the subtrees that match the labels
+                                    above with a node with no children labeled
+                                    with this value.
+
+  Phase: post
+  (same as pre)
+
+  Phase: generalize
+    match=val1, val2, ..., valN  :: seek nodes with one of these
+    target=val1, val2, ..., valM :: generalize one of these under a match 
+
+  Phase: context
+    match=val1, val2, ..., valN  :: seek nodes with one of these
+-}
 module RuleGen.Util.Configuration (
   ConfigurationRule (..),
   PreFilterRule (..),
@@ -12,6 +56,11 @@ import Data.ConfigFile
 import RuleGen.Data.Labels
 import RuleGen.Util.Misc
 
+{-|
+  Single data type representing any type of rule.  All rules come in
+  one big mixed up config file, so we use this to hold them all
+  until we partition them into classes later.
+-}
 data ConfigurationRule =
     PreFilter PreFilterRule
   | PostFilter PostFilterRule
@@ -19,6 +68,11 @@ data ConfigurationRule =
   | ContextualizeFilter ContextualizeFilterRule
   deriving (Show, Eq)
 
+{-|
+  Given a configuration rule set, partition it into four sets for each of
+  the legal rule types.  The type checker will make sure only valid members
+  occur in each set.
+-}
 partitionRuleSets :: [ConfigurationRule] -> 
                      ([PreFilterRule], [PostFilterRule], 
                       [GeneralizeFilterRule], [ContextualizeFilterRule])
@@ -33,7 +87,6 @@ partitionRuleSets cfg =
     (r:rs) = cfg
     (prs,pos,gs,cs) = partitionRuleSets rs
 
-
 data PreFilterRule = PreFilterRule LabelSet Label
   deriving (Show, Eq)
 
@@ -46,6 +99,12 @@ data GeneralizeFilterRule = GeneralizeFilterRule LabelSet [Label]
 data ContextualizeFilterRule = ContextualizeFilterRule LabelSet
   deriving (Show, Eq)
 
+{-|
+  Read config file.  This does not gracefully deal with
+  error conditions.  
+
+  TODO: make graceful with respect to error conditions
+-}
 readConfig :: String -> IO [ConfigurationRule]
 readConfig fname = do
     val <- readfile emptyCP fname
